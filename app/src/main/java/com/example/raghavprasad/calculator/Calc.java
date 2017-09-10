cfba98aa4d69a629479b2533f6359d91a88d7136
raghavprasad13 <31107116+raghavprasad13@users.noreply.github.com>
2017-09-10 18:49:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/raghavprasad/calculator/Calc.java
@@ -0,0 +1,18 @@
+package com.example.raghavprasad.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.text.DecimalFormat;
+
+public class Calc extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc);
+    }
+
+
+}
